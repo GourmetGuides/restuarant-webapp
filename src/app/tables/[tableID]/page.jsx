b1d937abafb5dfebe07509d3c9c589ce2a392723
Rishi Patel <Patel.Rishi@ibm.com>
2023-09-19 13:18:43 +0530

    added starting code

---
+++ src/app/tables/[tableID]/page.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TableID = ({params}) => {
+  return (
+    <div>
+      <h1>TableID</h1>
+      <h2>{params.tableID}</h2>
+    </div>
+  );
+};
+
+export default TableID;
